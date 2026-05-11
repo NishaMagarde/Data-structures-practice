e1bc993d4a6b1ccd8df69bcbffdfd9d09ab7defa
Nisha Magarde <isha.adlak009@gmail.com>
2026-05-11 22:17:14 +0530

    Data structures

---
+++ src/TwoSumArray.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class TwoSumArray {
+    public static int[] twoSum(int nums[] , int target) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+
+            int j = target-nums[i];
+            if(map.containsKey(j)){
+                int ans[] = {map.get(j),i};
+                return ans;
+            }
+            map.put(nums[i],i);
+        }
+        return new int[]{ };
+    }
+
+    public static void main(String[] args) {
+        int nums[] = {2,7,11,15};
+        int target = 9;
+        int[] result = twoSum(nums,target);
+        System.out.println(Arrays.toString(result));
+    }
+}
